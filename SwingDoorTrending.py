3d4d9ddabdb046cadd337133a021e6188a47dcbf
randomseed42 <randomseed42@protonmail.com>
2021-01-19 15:15:38 +0800

    correct when x is not index

--- SwingDoorTrending.py
+++ SwingDoorTrending.py
@@ -7,9 +7,9 @@ def swing_door_trending(x, y, e):
     _lower = y[0] - e
     max_angle_upper = 0
     max_angle_lower = 0
-    x_new = [0]
+    keep = [0]
     for i in range(1, len(_y)):
-        x_dist = _x[i] - x_new[-1]
+        x_dist = _x[i] - _x[keep[-1]]
         angle_upper = 90 + np.arctan( (_y[i] - _upper)/x_dist )*180/np.pi
         angle_lower = 90 - np.arctan( (_y[i] - _lower)/x_dist )*180/np.pi
         max_angle_upper = max(max_angle_upper, angle_upper)
@@ -19,5 +19,5 @@ def swing_door_trending(x, y, e):
             max_angle_lower = 0
             _upper = y[i] + e
             _lower = y[i] - e
-            x_new.append(i)
-    return x_new, _y[x_new]
+            keep.append(i)
+    return keep, _x[keep], _y[keep]
