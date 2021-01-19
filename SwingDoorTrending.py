655459c99a87afef8949f5ccab8594b1be673158
randomseed42 <randomseed42@protonmail.com>
2021-01-19 16:11:41 +0800

    added quantiles as m so extreme points can be kept

--- SwingDoorTrending.py
+++ SwingDoorTrending.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def swing_door_trending(x, y, e):
+def swing_door_trending(x, y, e, m=(25, 75)):
     _x = np.array(x)
     _y = np.array(y)
     _upper = _y[0] + e
@@ -14,7 +14,11 @@ def swing_door_trending(x, y, e):
         angle_lower = 90 - np.arctan( (_y[i] - _lower)/x_dist )*180/np.pi
         max_angle_upper = max(max_angle_upper, angle_upper)
         max_angle_lower = max(max_angle_lower, angle_lower)
-        if (max_angle_upper + max_angle_lower >= 180) | (i == (len(_y)-1)):
+        if ((max_angle_upper + max_angle_lower >= 180) 
+            | (i == (len(_y)-1))
+            | (_y[i] < np.percentile(_y, m[0]))
+            | (_y[i] > np.percentile(_y, m[1]))
+            ):
             max_angle_upper = 0
             max_angle_lower = 0
             _upper = _y[i] + e
